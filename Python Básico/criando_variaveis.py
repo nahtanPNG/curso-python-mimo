fd433fd27449ccc868b62f477a243bd6b5355706
nahtanpng <nathan.ferreiira13@gmail.com>
2023-07-30 19:23:07 -0300

    Python Basico

---
+++ Python Básico/criando_variaveis.py
@@ -0,0 +1,16 @@
+# Lição 1
+price = 239
+half_price = price / 2
+print(half_price)
+
+#Lição 2
+car_1 = "Porshe"
+city = "Miami"
+device_type = "apple"
+
+#Lição 3
+greeting = "Hello World!"
+print(greeting)
+print("3, 2, 1")
+print("GO!")
+
---
+++ Python Básico/usando_variaveis.py
@@ -0,0 +1,13 @@
+# Aula 1
+status = "Watching Netflix"
+status = "Relaxing at the beach"
+print(status)
+
+# Aula 2
+followers = "55"
+print ("Followers: " + followers)
+
+# Aula 3
+number_of_steps = 70
+print("You're on step:")
+print(number_of_steps + 1)
